f28213e6ccac4d074b16248639e3b256c1c46d12
Levon Ritter <levong2112@gmail.com>
2021-06-12 19:14:53 -0400

    successfully restyled footer

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,33 +1,15 @@
 import '../styles/style.css'
 import Heading from './heading'
+import Navbar from './navbar'
+import Subscribe from './subscribe'
+import Footer from './footer'
 
 function App() {
   return (
     <div id="container">
       <div id="top">
-          <Heading />
-          <div id="navbar">
-              <nav>
-                      <a id="home" href="index.html">
-                          Domum
-                      </a>
-                      <a class="navLink" href>
-                          Ludis
-                      </a>
-                      <a class="navLink" href>
-                          Imperatoria
-                      </a>
-                      <a class="navLink" href>
-                          Senatus
-                      </a>
-                      <a class="navLink" href>
-                          Cibus
-                      </a>
-                      <a class="navLink" href>
-                          Religio
-                      </a>
-              </nav> 
-          </div>                           
+            <Heading />
+            <Navbar />                      
       </div>
       <div id="middle">
           <div id="articles">
@@ -61,43 +43,12 @@ function App() {
                   <li>Deus Ex Machina</li>
               </ol>
           </div>
-          <div id="subscribe">
-              <h3 id="subRequest">Subscribe to our newsletter</h3>
-              <p>Choose a subscription type:</p>
-              <div id="subTypes">
-                  <span class="subType">
-                      <input type="checkbox" id="weekly" /> 
-                      <label for="weekly">Weekly</label>
-                  </span>
-                  <br/>
-                  <span class="subType">
-                      <input type="checkbox" id="daily" /> 
-                      <label for="daily">Daily</label>
-                  </span>
-              </div>  
-              <button id="btnSubscribe">
-                  Subscribe
-              </button>      
-          </div>
-      </div>
-      <div id="bottom">
           
-          <div id="footer">
-              <nav>
-                  <a class="navLinkBottom" href="/">
-                      Domum
-                  </a>
-                  <a class="navLinkBottom" href="/about">
-                      About
-                  </a>
-                  <a class="navLinkBottom" href>
-                      Contact
-                  </a>
-              </nav> 
-          </div>
-          <span id="disclaimer">
-              This is not a real news site. This is just a page I wrote to practice LESS with © 2021 Levon Ritter
-          </span>
+      </div>
+      <div>
+          <Subscribe />
+          <Footer />
+          
       </div>  
   </div>)
 }
